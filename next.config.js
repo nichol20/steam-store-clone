9822241cf790e12d75f6b01997921bf2fbaa0c68
nichol20 <nick.nogue28@outlook.com>
2022-05-25 14:36:32 -0300

    adding production url in next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['cdn.akamai.steamstatic.com', 'localhost']
+    domains: ['cdn.akamai.steamstatic.com', 'localhost', 'steam-store-clone.vercel.app']
   }
 }
 
